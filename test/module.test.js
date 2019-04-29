96d6c085c6748f23e82e3686401e2e98b75f3c2f
David Royer <droyer01@gmail.com>
2019-04-29 05:28:31 -0400

    chore: testing CI

--- test/module.test.js
+++ test/module.test.js
@@ -1,5 +1,5 @@
 jest.setTimeout(60000)
-process.env.PORT = process.env.PORT || 5060
+// process.env.PORT = process.env.PORT || 5060
 
 const { Nuxt, Builder } = require('nuxt-edge')
 const request = require('request-promise-native')
@@ -15,10 +15,16 @@ const get = path => request(url(path))
 
 describe('basic', () => {
   beforeAll(async () => {
+    // eslint-disable-next-line no-console
+    console.log('INSIDE beforeAll')
+
     nuxt = new Nuxt(config)
     await nuxt.ready()
     await new Builder(nuxt).build()
     port = await getPort()
+
+    // eslint-disable-next-line no-console
+    console.log('TCL: port', port)
     await nuxt.listen(port)
 
     // config.dev = false

580bf6f4f46153341ae3fbe1fb926cb63dbb18a5
David Royer <droyer01@gmail.com>
2021-01-31 10:31:18 -0500

    fix: fixes all current issues

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -4,6 +4,7 @@ const { resolve } = require('path')
 // eslint-disable-next-line no-console
 
 module.exports = {
+  target: 'static',
   rootDir: resolve(__dirname, '..'),
   buildDIr: resolve(__dirname, '.nuxt'),
   srcDir: __dirname,
@@ -19,18 +20,7 @@ module.exports = {
 
   generate: {
     fallback: true
-    // routes: () => {
-    //   const blogRoutes = cmsRouteGenerator(
-    //     'articles',
-    //     require(`./_API/articles`)
-    //   )
-    //   const projectRoutes = cmsRouteGenerator(
-    //     'projects',
-    //     require(`./_API/projects`)
-    //   )
 
-    //   return [...blogRoutes, ...projectRoutes]
-    // }
   },
 
   build: {
